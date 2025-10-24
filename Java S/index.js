9a1ec08cde8d12ab8a0c8797f3f9ec0e265c0f2b
mateorodriguez87 <mateorodriguezepet20@gmail.com>
2025-10-24 19:05:46 -0300

    Agregué getelementbyid cerrar

--- Java S/index.js
+++ Java S/index.js
@@ -1,4 +1,5 @@
 const titulo = document.querySelector('.animar');
 const modal = document.getElementById('modal');
 const mensaje = document.getElementById('mensaje');
+const cerrar = document.getElementById('cerrar');
 
