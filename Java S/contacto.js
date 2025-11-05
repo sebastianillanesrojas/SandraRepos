65909d6312ab5cdb02c4db04a9a6e1d611757724
Daniela0701 <danielafernandariquelme0107@gmail.com>
2025-11-05 17:27:29 -0300

    Agregué mouseover y mouseout a contacto

---
+++ Java S/contacto.js
@@ -0,0 +1,10 @@
+var titulo = document.getElementById("titulo");
+
+// 
+titulo.addEventListener("mouseover", (event) => {
+    titulo.style.backgroundColor="blue";
+ })
+
+titulo.addEventListener("mouseout", (event) => {
+    titulo.style.backgroundColor="white"; #68ba2dff
+ })
