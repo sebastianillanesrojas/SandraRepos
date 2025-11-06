735e916d56b3b0641b29fb3dee19b991e5c91b59
sami2603 <samira.tejada.epet20@gmail.com>
2025-11-06 00:16:08 -0300

    Bùsca la palabra maps

--- Java S/envios.js
+++ Java S/envios.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     alert("¡Bienvenido a la sección de envíos de Sandra Repos!");
     const resaltados = document.querySelectorAll(".resaltado");
+
+      resaltados.forEach((item) => {
+    item.addEventListener("click", () => {
+      alert(" Recuerda avisar con una semana de anticipación si deseas modificar tu pedido.");
+    });
+  });
+
 })
