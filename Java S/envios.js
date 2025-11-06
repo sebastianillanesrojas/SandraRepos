d1e6332e9bf4046d35d444eb37c9064065b7ac23
sami2603 <samira.tejada.epet20@gmail.com>
2025-11-06 00:17:51 -0300

    Guardè el cambio

--- Java S/envios.js
+++ Java S/envios.js
@@ -2,10 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     alert("¡Bienvenido a la sección de envíos de Sandra Repos!");
     const resaltados = document.querySelectorAll(".resaltado");
 
-      resaltados.forEach((item) => {
-    item.addEventListener("click", () => {
-      alert(" Recuerda avisar con una semana de anticipación si deseas modificar tu pedido.");
+    const enlace = document.querySelector("a[href*='maps']");
+  enlace.addEventListener("click", (e) => {
+    
     });
-  });
-
 })
