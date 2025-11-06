ec8aaf3722ddd329ff6e29297d5edcef21614fff
sami2603 <samira.tejada.epet20@gmail.com>
2025-11-06 00:18:54 -0300

    Mostrar cartel

--- Java S/envios.js
+++ Java S/envios.js
@@ -4,6 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
     const enlace = document.querySelector("a[href*='maps']");
   enlace.addEventListener("click", (e) => {
-    
+    const confirmar = confirm("¿Deseas abrir la ubicación en Google Maps?");
     });
 })
