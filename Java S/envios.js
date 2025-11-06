ff20a9feeb2de58d165b3e351acffa159a2ec484
sami2603 <samira.tejada.epet20@gmail.com>
2025-11-06 00:01:47 -0300

    Selcciono los los elementos de clase resaltado

--- Java S/envios.js
+++ Java S/envios.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    
+    alert("¡Bienvenido a la sección de envíos de Sandra Repos!");
+    const resaltados = document.querySelectorAll(".resaltado");
 })
